d25fd56804412b70d1a99f9a429f720a984b6a04
maraj hussain <maraj.hussain75@gmail.com>
2025-03-03 17:28:10 +0530

    Initial Commit

---
+++ WordsPuzzleGame/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WordsPuzzleGame
+//
+//  Created by Maraj Hussain on 03/03/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WordsPuzzleGame/WordsPuzzleGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordsPuzzleGameApp.swift
+//  WordsPuzzleGame
+//
+//  Created by Maraj Hussain on 03/03/25.
+//
+
+import SwiftUI
+
+@main
+struct WordsPuzzleGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
